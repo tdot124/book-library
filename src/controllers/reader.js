40395079df25fb88c4cd4a54df8a0668fa12a68f
Tom Mills <t.mills124@gmail.com>
2022-08-21 16:21:24 +0100

    added ability to read readers table by ID

--- src/controllers/reader.js
+++ src/controllers/reader.js
@@ -3,11 +3,31 @@ const { Reader } = require('../models');
 exports.create = async (req, res) => {
     const newReader = await Reader.create(req.body);
 
-    res.status(201).json(newReader);
+    if (!newReader) {
+        res.status(404).json({error: 'reader not created'})
+    } else {
+        res.status(201).json(newReader);
+    }    
 };
 
 exports.readAll = async (req, res) => {
     const readers = await Reader.findAll();
 
+    if (!readers) {
+        res.status(404).json({error: 'Readers could not be found'})
+    } else {
     res.status(200).json(readers);
+    }
+}
+
+exports.readById = async (req, res) => {
+    const { readerId } = req.params;
+    const reader = await Reader.findByPk(readerId);
+    console.log(reader);
+
+    if (!reader) {
+        res.status(404).json({error: 'The reader could not be found.'});
+    } else {
+        res.status(200).json(reader);
+    }
 }
--- src/routes/reader.js
+++ src/routes/reader.js
@@ -4,6 +4,7 @@ const readerController = require('../controllers/reader');
 const router = express.Router();
 
 router.post('/',readerController.create);
-router.get('/',readerController.readAll)
+router.get('/', readerController.readAll);
+router.get('/:readerId', readerController.readById);
 
 module.exports = router;
